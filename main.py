ad16bf13a658fec2721b6b446f8f56ea99052549
PetrosyanRafayel <rafayel.petrosyan@majorel.com>
2026-02-07 20:55:58 +0400

    Initial commit

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 # Import your forms from the forms.py
 from forms import RegisterForm, LoginForm, CreatePostForm, CommentForm
 from sqlalchemy import ForeignKey
-
+import os
 
 
 
@@ -32,7 +32,7 @@ This will install the packages from the requirements.txt for this project.
 '''
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -42,7 +42,7 @@ Bootstrap5(app)
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
